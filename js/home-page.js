b205fc2a9d40720a11755dc67f6e7f5e33412dd0
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-08-15 15:48:10 +0200

    commit*

--- js/home-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-////Home page - load with delay
-const homePageContainer = document.querySelector('.general-container');
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(() => {
-    homePageContainer.style.opacity = '1';
-  }, 300);
-});
