a21c780aac3104dde57ea10f821a0b3f4cdee1ea
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-08-22 13:17:47 +0200

    Edited pickr functionality script
    *Now, There's one 'pickr' object in the script responsible for all pickers in the app. Pickr object gets created with the method document.createElement() and then is inserted into DOM.

---
+++ js/color-picker.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/////////////////////////////////////////////////
+////COLOR PICKERS FOR COLOR PALETTE GENERATOR
+
+//Displaying color pickr functionality in DOM
+const pickrLabels = document.querySelectorAll('.color-picker');
+
+export const insertClrPickers = () => {
+  pickrLabels.forEach(label => {
+    const colorPicker = document.createElement('button');
+    label.after(colorPicker);
+
+    const pickr = Pickr.create({
+      el: colorPicker,
+      theme: 'classic',
+
+      appClass: 'color-picker-custom-class',
+
+      components: {
+        preview: true,
+        opacity: false,
+        hue: true,
+
+        interaction: {
+          hex: false,
+          rgba: false,
+          hsla: false,
+          hsva: false,
+          cmyk: false,
+          input: true,
+          clear: false,
+          save: true,
+        },
+      },
+
+      i18n: {
+        'btn:save': 'PICK 😀',
+      },
+    });
+  });
+};
+
+insertClrPickers();
+
+//Applying color to DOM from color picker
+const saveBtns = document.querySelectorAll('.pcr-save');
+const results = document.querySelectorAll('.pcr-result');
+const colorItems = document.querySelectorAll('.color');
+const HEXlabels = document.querySelectorAll('.hex-label');
+const copyHEXbtn = document.querySelectorAll('.copy-hex-btn');
+const addToPaletteBtns = document.querySelectorAll('.add-to-palette-btn');
+
+const applyColorFromPicker = e => {
+  let target = e.target;
+  let hex = target.parentElement.children[0].value;
+  let clickedIndex = [...saveBtns].map(btn => [...saveBtns].indexOf(target))[0];
+
+  if (!colorItems[clickedIndex]) return;
+
+  if (!colorItems[clickedIndex].classList.contains('locked')) {
+    colorItems[clickedIndex].style.backgroundColor = hex;
+    HEXlabels[clickedIndex].textContent = hex;
+  } else {
+    colorItems[clickedIndex].style.backgroundColor = ' ';
+  }
+};
+
+saveBtns.forEach(btn => btn.addEventListener('click', applyColorFromPicker));
