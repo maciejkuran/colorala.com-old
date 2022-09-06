e2070ba91a420a85f5f53a961211d83f69377ad3
Maciej Kuran - Janowski <maciejkuran@gmail.com>
2022-09-06 19:16:23 +0200

    Added new color palettes

--- js/pre-made-palettes.js
+++ js/pre-made-palettes.js
@@ -25,6 +25,27 @@ const palette_4_dark = new Palette(
   ['All', 'Dark'],
   ['#494847', '#6E6E6E', '#F76526', '#FF7527']
 );
+const palette_5_dark = new Palette(
+  ['All', 'Dark'],
+  ['#2E3550', '#AA777B', '#E8B242', '#5CA059']
+);
+const palette_6_dark = new Palette(
+  ['All', 'Dark'],
+  ['#4260A7', '#39C5A8', '#E1205F', '#CDC938']
+);
+const palette_7_dark = new Palette(
+  ['All', 'Dark'],
+  ['#59B1BC', '#1EABA9', '#252A16', '#E1205F']
+);
+const palette_8_dark = new Palette(
+  ['All', 'Dark'],
+  ['#EB1050', '#A28674', '#56F6F7', '#241A42']
+);
+const palette_9_dark = new Palette(
+  ['All', 'Dark'],
+  ['#597545', '#292637', '#666D2E', '#F07D49']
+);
+
 //Light
 const palette_1_light = new Palette(
   ['All', 'Light'],
@@ -42,6 +63,19 @@ const palette_4_light = new Palette(
   ['All', 'Light'],
   ['#FFEFEF', '#FEEDFF', '#E8DBE8', '#F5D8E8']
 );
+const palette_5_light = new Palette(
+  ['All', 'Light'],
+  ['#F0F0F0', '#E1E1E1', '#C1C1C1', '#CECFDA']
+);
+const palette_6_light = new Palette(
+  ['All', 'Light'],
+  ['#AEEBFF', '#CAFFBD', '#D2FFFB', '#EDE7FF']
+);
+const palette_7_light = new Palette(
+  ['All', 'Light'],
+  ['#DDB7B1', '#E8ABBF', '#FFB3C6', '#CDD4BB']
+);
+
 //Mix
 const palette_1_mix = new Palette(
   ['All', 'Mix'],
@@ -66,4 +100,59 @@ const palette_5_mix = new Palette(
 const palette_6_mix = new Palette(
   ['All', 'Mix'],
   ['#303030', '#0068FF', '#E6E5D1', '#00FF70']
+);
+
+const palette_7_mix = new Palette(
+  ['All', 'Mix'],
+  ['#8C8E56', '#DCEBBB', '#A7A648', '#A96721']
+);
+
+const palette_8_mix = new Palette(
+  ['All', 'Mix'],
+  ['#314738', '#294335', '#DCEBBB', '#8C8E56']
+);
+
+const palette_9_mix = new Palette(
+  ['All', 'Mix'],
+  ['#DD2668', '#DFDFE3', '#2739FF', '#0015FF']
+);
+
+const palette_10_mix = new Palette(
+  ['All', 'Mix'],
+  ['#F9BBF4', '#DF50DC', '#D13CB0', '#BD20C9']
+);
+
+const palette_11_mix = new Palette(
+  ['All', 'Mix'],
+  ['#676F62', '#2D10A9', '#646EC6', '#A0B2F3']
+);
+
+const palette_12_mix = new Palette(
+  ['All', 'Mix'],
+  ['#687171', '#9BCDBC', '#859A73', '#1672DC']
+);
+
+const palette_13_mix = new Palette(
+  ['All', 'Mix'],
+  ['#B03444', '#9BCDBC', '#92E6D7', '#5D8E95']
+);
+
+const palette_14_mix = new Palette(
+  ['All', 'Mix'],
+  ['#EDA836', '#9BCDBC', '#92E6D7', '#C8B3BC']
+);
+
+const palette_15_mix = new Palette(
+  ['All', 'Mix'],
+  ['#DDB7B1', '#CC7A85', '#CDD4BB', '#405E97']
+);
+
+const palette_16_mix = new Palette(
+  ['All', 'Mix'],
+  ['#E9BB50', '#E99643', '#CDD4BB', '#DDB7B1']
+);
+
+const palette_17_mix = new Palette(
+  ['All', 'Mix'],
+  ['#E1CFD5', '#15F59B', '#25F7AC', '#E41123']
 );
